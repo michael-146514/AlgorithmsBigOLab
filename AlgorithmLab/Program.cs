1780a86bff29d08858f1f8c71335774966297111
michael-146514 <santamariamichael5@gmail.com>
2023-08-08 11:25:29 -0500

    First push to github, Algorithm Lab

---
+++ AlgorithmLab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
